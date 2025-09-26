3ee6538f16cff1dfac584c2ff4865b8ff944fd3f
@Maival1 <maival.cimba@gmail.com>
2025-09-26 11:05:01 +0300

    fix: hide types

--- src/shared/ui/Dots/Dots.tsx
+++ src/shared/ui/Dots/Dots.tsx
@@ -24,6 +24,7 @@ export function Dots({
   return (
     <div
       className={clsx(s.root, className)}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       style={{ ["--dot-size" as any]: `${size}px`, ["--dot-gap" as any]: `${gap}px` }}
       role="tablist"
       aria-label="Индикатор слайдов"
